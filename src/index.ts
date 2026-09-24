4817f5940558f2b3bf0b42e09ed8da91dc3e62a7
ManauTunjae <manau.tunjae@hotmail.com>
2026-09-24 20:40:23 +0200

    feat: add route for properties in the Hono application

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import properties from './routes/properties.js'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.route("/properties", properties);
 
 serve({
   fetch: app.fetch,
